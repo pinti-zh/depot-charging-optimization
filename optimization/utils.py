81e4d89a0b3dfa557a1195170fc06911ad1e7607
pinti <pinterl@protonmail.com>
2025-06-13 12:50:06 +0200

    utils refactor

--- optimization/utils.py
+++ optimization/utils.py
@@ -1,12 +1,6 @@
 import re
 from typing import Iterable, TypeVar
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-from gurobipy import GRB
-from matplotlib.patches import Rectangle
-from rich import print
-
 T = TypeVar("T")
 
 
@@ -18,75 +12,11 @@ def list_start_string(values: Iterable, num: int):
         return s[:-1] + ", ...]"
 
 
-def plot_solution(opt_model):
-    num_time_steps = len(opt_model.data)
-    time_steps = [i * opt_model.dt for i in range(num_time_steps + 1)]
-    cap = opt_model.data["battery_capacity"][0]
-    sns.set_style("dark")
-    fig, axes = plt.subplots(2, height_ratios=[2, 1], figsize=(12, 8))
-    fig.subplots_adjust(top=0.92)
-    fig.suptitle("Optimization Result", fontsize=16)
-    twin_axis = axes[0].twinx()
-    twin_axis.bar(
-        [(i + 0.5) * opt_model.dt for i in opt_model.charging_indices],
-        [cp.X * 3600 for cp in opt_model.charging_power],
-        color="forestgreen",
-        width=opt_model.dt,
-        label="Charging Power",
-        zorder=1.2,
-    )
-    axes[0].plot(
-        time_steps,
-        [soe.X for soe in opt_model.state_of_energy],
-        color="navy",
-    )
-    axes[0].plot(
-        time_steps,
-        [cap * 0.2] * (num_time_steps + 1),
-        color="navy",
-        linestyle="dashed",
-        label="SoE Lower Bound",
-    )
-    axes[0].plot(
-        time_steps,
-        [cap * 0.8] * (num_time_steps + 1),
-        color="navy",
-        linestyle="dashed",
-        label="SoE Upper Bound",
-    )
-    for time, mcp in zip(opt_model.data["time"], opt_model.data["max_charging_power"]):
-        twin_axis.plot(
-            [time - opt_model.dt, time],
-            [mcp, mcp],
-            color="forestgreen",
-        )
-    for time, depot_charge in zip(opt_model.data["time"], opt_model.data["depot_charge"]):
-        if depot_charge and False:
-            axes[0].add_patch(
-                Rectangle((time - opt_model.dt, 0), opt_model.dt, cap, color="forestgreen", alpha=0.2, linewidth=None)
-            )
-    axes[0].legend()
-    twin_axis.legend()
-    axes[0].set_ylim(0, cap)
-    axes[0].set_xlim(0, max(opt_model.data["time"]))
-    axes[0].set_xlabel("Time [s]")
-    axes[0].set_ylabel("SoE [kWh]")
-    twin_axis.set_ylabel("Charing Power [kW]")
-    energy_time_steps = [0] + [t for t in time_steps[1:-1] for _ in (0, 1)] + [time_steps[-1]]
-    energy_price_values = [val for val in opt_model.energy_price["energy_price"] for _ in (0, 1)]
-    axes[1].plot(energy_time_steps, energy_price_values, color="darkmagenta", label="Energy Price")
-    axes[1].set_xlim(0, max(opt_model.data["time"]))
-    axes[1].legend()
-    axes[1].set_xlabel("Time [s]")
-    axes[1].set_ylabel("Price [Chf]")
-    plt.show()
-
-
-def atoi(text):
+def atoi(text: str) -> int | str:
     return int(text) if text.isdigit() else text
 
 
-def natural_keys(text):
+def natural_keys(text: str) -> list:
     """
     alist.sort(key=natural_keys) sorts in human order
     http://nedbatchelder.com/blog/200712/human_sorting.html
@@ -95,116 +25,11 @@ def natural_keys(text):
     return [atoi(c) for c in re.split(r"(\d+)", text)]
 
 
-def print_linear_constraints(model):
-    constraints = model.getConstrs()
-    print("[bold]Linear Constraints:")
-    max_name_len = max(len(constraint.ConstrName) for constraint in constraints)
-    max_constraint_len = max(len(str(model.getRow(constraint))) for constraint in constraints)
-    for constraint in sorted(constraints, key=lambda x: natural_keys(x.ConstrName)):
-        match constraint.Sense:
-            case GRB.LESS_EQUAL:
-                sense = "[bold]<=[/bold]"
-            case GRB.EQUAL:
-                sense = "[bold]==[/bold]"
-            case GRB.GREATER_EQUAL:
-                sense = "[bold]>=[/bold]"
-            case _:
-                sense = "???"
-        name_buffer = (max_name_len - len(constraint.ConstrName)) * " "
-        print(
-            f"  {name_buffer}[bright_blue italic]{constraint.ConstrName}[/bright_blue italic]: ",
-            end="",
-        )
-        constraint_buffer = (max_constraint_len - len(str(model.getRow(constraint)))) * " "
-        print(f"{constraint_buffer}{model.getRow(constraint)} {sense} {constraint.RHS}")
-
-
-def print_general_constraints(model):
-    constraints = model.getGenConstrs()
-    print("[bold]General Constraints:")
-    max_constraint_len = max(len(constraint.genConstrName) for constraint in constraints)
-    for constraint in constraints:
-        name_buffer = (max_constraint_len - len(constraint.genConstrName)) * " "
-        print(f"  {name_buffer}[bright_blue italic]{constraint.genConstrName}")
-
-
-def print_variables(model):
-    print("[bold]Variable Bounds:")
-    max_variable_len = max(len(variable.VarName) for variable in model.getVars())
-    max_lower_bound_len = max(len(str(variable.LB)) for variable in model.getVars())
-    for variable in sorted(model.getVars(), key=lambda x: natural_keys(x.VarName)):
-        variable_buffer = (max_variable_len - len(variable.VarName)) * " "
-        lb_buffer = (max_lower_bound_len - len(str(variable.LB))) * " "
-        print(f"  {lb_buffer}{variable.LB} [bold]<=[/bold] ", end="")
-        print(f"[bright_blue italic]{variable_buffer}{variable.VarName}[/bright_blue italic] ", end="")
-        print(f"[bold]<=[/bold] {variable.UB}")
-
-
-def print_model_summary(opt_model, verbosity=1):
-    if verbosity >= 1:
-        opt_model.model.setParam("OutputFlag", 1)
-        print("[bold]" + "=" * 100)
-        print(" " * 40 + "[bold]OPTIMIZATION PROBLEM" + 40 * " ")
-        print("[bold]" + "=" * 100)
-        opt_model.model.printStats()
-        if verbosity >= 2:
-            print("[bold]" + "-" * 100)
-
-            if len(opt_model.model.getConstrs()):
-                print_linear_constraints(opt_model.model)
-                print()
-            if len(opt_model.model.getGenConstrs()):
-                print_general_constraints(opt_model.model)
-                print()
-            if len(opt_model.model.getVars()):
-                print_variables(opt_model.model)
-
-        print("[bold]" + "=" * 100)
-        opt_model.model.setParam("OutputFlag", 0)
-
-
-def print_solution(opt_model, verbosity=1):
-    if verbosity >= 2:
-        soe_strings = list(map(lambda x: f"{x.X:.2f}", opt_model.state_of_energy))
-        cp_strings = []
-        cp_index = 0
-        for i in range(len(opt_model.data)):
-            if i in opt_model.charging_indices:
-                cp_strings.append(f"{opt_model.charging_power[cp_index].X:.4f}")
-                cp_index += 1
-            else:
-                cp_strings.append(" ")
-        max_string_len = max(max(map(len, soe_strings)), max(map(len, cp_strings)))
-        max_string_len = max(max(map(len, soe_strings)), max(map(len, cp_strings)))
-        for soe_string in soe_strings:
-            buffer = " " * (max_string_len - len(soe_string))
-            print(f"[bold blue]{buffer}{soe_string}{' ' * max_string_len}", end="")
-        print()
-        for cp_string in cp_strings:
-            buffer = " " * (max_string_len - len(cp_string))
-            print(f"[bold red]{' ' * max_string_len}{buffer}{cp_string}", end="")
-        print(
-            f"[bold red]{' ' * max_string_len} -> {sum([cp.X * opt_model.dt for cp in opt_model.charging_power]):.2f}kWh"
-        )
-        print("[bold]" + "-" * 100)
-    if verbosity >= 1:
-        grid_cost = opt_model.max_charging_power.X * opt_model.power_grid_tariff
-        energy_cost = sum(
-            opt_model.energy_price["energy_price"][i] * cp.X * opt_model.dt
-            for i, cp in zip(opt_model.charging_indices, opt_model.charging_power)
-        )
-        print(f"Energy Cost:        [bold green]{energy_cost:.2f}$")
-        print(f"Grid Cost:        + [bold green]{grid_cost:.2f}$")
-        print("-------------------------")
-        print(f"Total Optimal Cost: [bold green]{opt_model.model.ObjVal:.2f}$")
-    else:
-        print(opt_model.solution)
-
-
 def expand_values(time: Iterable[int], values: Iterable[T], granularity: int, interpolation: str = "same") -> list[T]:
     expanded_values = []
     current_time = 0
     current_value = 0
+    eps = 1e-6
     for t, v in zip(time, values):
         assert t >= current_time
         assert t % granularity == 0
@@ -218,7 +43,7 @@ def expand_values(time: Iterable[int], values: Iterable[T], granularity: int, in
                 ev = int(ev)
             else:
                 ev = float(ev)
-            assert ev * num == v
+            assert abs(ev * num - v) < eps
             expanded_values += [ev] * num
         elif interpolation == "linear" and type(v) in [float, int]:
             unit = (v - current_value) / num
