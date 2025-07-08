a2bed547ddc98fd8f1b07250a66b1190e15a9436
pinti <pinterl@protonmail.com>
2025-07-08 14:56:48 +0200

    refactor: pandas and logging

--- src/depot_charging_optimization/scripts/clean_data.py
+++ src/depot_charging_optimization/scripts/clean_data.py
@@ -1,9 +1,15 @@
+import logging
 import os
-from typing import Optional
 
-import polars as pl
 import click
-from rich import print as printr
+import pandas as pd
+from rich.logging import RichHandler
+
+logging.basicConfig(
+    level="INFO", format="%(message)s", datefmt="[%X]", handlers=[RichHandler(markup=True)]
+)  # or DEBUG
+
+logger = logging.getLogger("optimize")
 
 
 def is_comma_float(string):
@@ -30,38 +36,34 @@ def add_decimal_point_to_int(string):
 @click.argument("target", type=str)
 @click.option("--sep", "-s", type=str, default=";", help="csv file seperator")
 def clean_data(source, target, sep):
-    data = pl.read_csv(source, separator=sep, infer_schema=False)
+    df = pd.read_csv(source, sep=sep, dtype=str, keep_default_na=False)  # read data as string
 
-    printr(f"loaded [magenta]{source}[/magenta]\n  {len(data.columns)} columns and {len(data)} rows")
-    printr("  head", data.head())
+    logger.info(f"loaded [magenta]{source}[/magenta]\n  {len(df.columns)} columns and {len(df)} rows")
 
     none_value_placeholders = ["NA"]
-    for col in data.columns:
+    for col in df.columns:
         found_placeholder = False
         found_comma_floats = False
         for none_value_placeholder in none_value_placeholders:
-            if none_value_placeholder in data[col]:
+            if none_value_placeholder in df[col]:
                 found_placeholder = True
-                data = data.with_columns(pl.col(col).replace(none_value_placeholder, None))
-        if any(data[col].map_elements(is_comma_float, return_dtype=pl.Boolean)):
+                df[col] = df[col].replace(none_value_placeholder, None)
+        if any(df[col].map(is_comma_float)):
             found_comma_floats = True
-            data = data.with_columns(pl.col(col).str.replace(",", "."))
-            data = data.with_columns(
-                pl.col(col).map_elements(add_decimal_point_to_int, return_dtype=Optional[pl.String])
-            )
-        printr(f"    column [cyan]{col}[/cyan]  ", end="")
+            df[col] = df[col].replace(",", ".")
+            df[col] = df[col].map(add_decimal_point_to_int)
         if not (found_placeholder or found_comma_floats):
-            printr("[bold green]ok")
+            logger.info(f"  column [cyan]{col}[/cyan] [bold green]ok")
         elif found_placeholder and not found_comma_floats:
-            printr("[gold1]replaced none value placeholders")
+            logger.info(f"  column [cyan]{col}[/cyan] [gold1]replaced none value placeholders")
         elif not found_placeholder and found_comma_floats:
-            printr("[orange1]replaced floats that had comma decimal point")
+            logger.info(f"  column [cyan]{col}[/cyan] [orange1]replaced floats that had comma decimal point")
         elif found_placeholder and found_comma_floats:
-            printr(
-                "[gold1]replaced none value placeholders [italic white]and [orange1]replaced floats that had comma decimal point"
+            logger.info(
+                f"  column [cyan]{col}[/cyan] [gold1]replaced none value placeholders [italic white]and [orange1]replaced floats that had comma decimal point"
             )
     output_dir = os.path.dirname(target)
     os.makedirs(output_dir, exist_ok=True)
-    data.write_csv(target)
+    df.to_csv(target, index=False)
 
-    printr(f"saved data as [green]{target}")
+    logger.info(f"saved data as [green]{target}")
