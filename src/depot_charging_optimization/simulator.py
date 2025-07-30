3d408aad53c6655c0535bef3c183e7cf8e685f59
pinti <pinterl@protonmail.com>
2025-07-30 10:42:08 +0200

    feat: peak shaving simulator

--- src/depot_charging_optimization/simulator.py
+++ src/depot_charging_optimization/simulator.py
@@ -125,6 +125,54 @@ class GreedySimulator(Simulator):
         )
 
 
+class PeakShavingSimulator(Simulator):
+    def __init__(self, input_data: Input, max_power: float):
+        super().__init__(input_data)
+        self._timestamp_map: list[list[int]] = []
+        self._max_power = max_power
+
+    def _setup(self, **kwargs) -> None:
+        for vehicle in range(self.input_data.num_vehicles):
+            self._state_of_energy.append([0.0 for _ in range(self.input_data.num_timesteps + 1)])
+            self._charging_power.append([0.0 for _ in range(self.input_data.num_timesteps)])
+            self._effective_charging_power.append([0.0 for _ in range(self.input_data.num_timesteps)])
+            t_start = first_depot_departure(self.input_data.depot_charge[vehicle])
+            self._timestamp_map.append(
+                [(t_i + t_start) % self.input_data.num_timesteps for t_i in range(self.input_data.num_timesteps)]
+            )
+            self._state_of_energy[vehicle][t_start] = 0.8 * self.input_data.battery_capacity[vehicle]
+
+    def _on_timestep(self, t_i, ce_function_type="one", alpha=1.0, **kwargs) -> None:
+        for vehicle in range(self.input_data.num_vehicles):
+            t_m = self._timestamp_map[vehicle][t_i]
+            next_soe = self._state_of_energy[vehicle][t_m] - self.input_data.energy_demand[vehicle][t_m]
+            max_soe = 0.8 * self.input_data.battery_capacity[vehicle]
+            if self.input_data.depot_charge[vehicle][t_m] and (next_soe < max_soe):
+                tund = time_until_next_departure(self._delta_time, self.input_data.depot_charge[vehicle], t_m)
+                desired_power = (max_soe - next_soe) / tund
+                cp, ecp = power_and_effective_power(desired_power, self._max_power, ce_function_type, alpha)
+                next_soe += ecp * self._delta_time[t_m]
+                self._charging_power[vehicle][t_m] = cp
+                self._effective_charging_power[vehicle][t_m] = ecp
+            t_next = self._timestamp_map[vehicle][(t_i + 1) % self.input_data.num_timesteps]
+            self._state_of_energy[vehicle][t_next] = next_soe
+
+    def _post_processing(self, **kwargs) -> None:
+        for vehicle in range(self.input_data.num_vehicles):
+            self._state_of_energy[vehicle][-1] = self._state_of_energy[vehicle][0]
+        self._solution = Solution(
+            input_data=self.input_data,
+            total_cost=self.energy_cost + self.power_cost,
+            energy_cost=self.energy_cost,
+            power_cost=self.power_cost,
+            gap=0.0,
+            max_charging_power_used=self.max_charging_power,
+            charging_power=self.charging_power,
+            effective_charging_power=self.effective_charging_power,
+            state_of_energy=self.state_of_energy,
+        )
+
+
 def first_depot_departure(depot_charge) -> int:
     state = False
     for t_i, dc in enumerate(depot_charge):
@@ -154,3 +202,14 @@ def power_and_effective_power(
             return cp, desired_power
     else:
         raise ValueError(f"Unknown ce_function_type: {ce_function_type}")
+
+
+def time_until_next_departure(delta_time: list[float], depot_charge: list[bool], start_time_index: int) -> int | float:
+    dt = 0
+    t_i = start_time_index
+    for _ in range(len(delta_time)):
+        if not depot_charge[t_i % len(depot_charge)]:
+            return dt
+        dt += delta_time[t_i % len(delta_time)]
+        t_i += 1
+    return float("inf")
--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from depot_charging_optimization.data_models import Input
-from depot_charging_optimization.simulator import GreedySimulator
+from depot_charging_optimization.simulator import GreedySimulator, PeakShavingSimulator
 
 EPS = 1e-4
 
@@ -59,3 +59,58 @@ class TestGreedySimulator:
 
         solution = simulator.run()
         assert abs(solution.total_cost - 195.0) < EPS
+
+
+class TestPeakShavingSimulator:
+    def test_simple(self):
+        df = pd.DataFrame(
+            {
+                "time": [5, 10, 15, 20],
+                "energy_demand": [0.0, 30.0, 0.0, 0.0],
+                "depot_charge": [True, False, True, True],
+                "battery_capacity": [50.0, 50.0, 50.0, 50.0],
+                "max_charging_power": [6.0, 0.0, 6.0, 6.0],
+            }
+        )
+        energy_price = pd.DataFrame({"time": [5, 10, 15, 20], "energy_price": [1.0, 0.0, 2.0, 1.0]})
+        grid_tariff = 1.0
+
+        input_data = Input.from_dataframe(df)
+        input_data = input_data.add_energy_price(
+            energy_price["time"].to_list(), energy_price["energy_price"].to_list()
+        )
+        input_data = input_data.add_grid_tariff(grid_tariff)
+
+        simulator = PeakShavingSimulator(input_data, 6.0)
+
+        solution = simulator.run()
+        assert abs(solution.total_cost - 42.0) < EPS
+
+    def test_complex(self):
+        df = pd.DataFrame(
+            {
+                "time": [5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+                "energy_demand": [0.0, 20.0, 10.0, 0.0, 10.0, 0.0, 0.0, 20.0, 0.0, 0.0],
+                "depot_charge": [True, False, False, True, False, True, True, False, True, True],
+                "battery_capacity": [50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0],
+                "max_charging_power": [5.0, 0.0, 5.0, 5.0, 5.0, 5.0, 5.0, 0.0, 5.0, 5.0],
+            }
+        )
+        energy_price = pd.DataFrame(
+            {
+                "time": [5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+                "energy_price": [1.0, 0.0, 2.0, 2.0, 1.0, 3.0, 1.0, 1.0, 5.0, 2.0],
+            }
+        )
+        grid_tariff = 1.0
+
+        input_data = Input.from_dataframe(df)
+        input_data = input_data.add_energy_price(
+            energy_price["time"].to_list(), energy_price["energy_price"].to_list()
+        )
+        input_data = input_data.add_grid_tariff(grid_tariff)
+
+        simulator = PeakShavingSimulator(input_data, 4.0)
+
+        solution = simulator.run()
+        assert abs(solution.total_cost - 412.0 / 3.0) < EPS
