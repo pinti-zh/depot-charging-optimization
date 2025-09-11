c3b82b1cc9f84501b60f12e1e191ccf5d4dad764
pinti <pinterl@protonmail.com>
2025-09-11 12:17:20 +0200

    fix: repeat strategy

--- src/depot_charging_optimization/controller.py
+++ src/depot_charging_optimization/controller.py
@@ -2,4 +2,4 @@ from depot_charging_optimization.data_models import Solution
 
 
 def policy_from_solution(solution: Solution, num_steps: int) -> list[list[float | None]]:
-    return [[cp[i] for cp in solution.charging_power] for i in range(num_steps)]
+    return [[cp[i % len(cp)] for cp in solution.charging_power] for i in range(num_steps)]
