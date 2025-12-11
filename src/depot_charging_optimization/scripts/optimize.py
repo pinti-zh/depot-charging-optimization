ef40beb6bba832d87f7c0e0832f888535b8dbcd7
pinti <pinterl@protonmail.com>
2025-12-11 12:41:45 +0100

    refactor: optimizer script

--- src/depot_charging_optimization/scripts/optimize.py
+++ src/depot_charging_optimization/scripts/optimize.py
@@ -1,6 +1,5 @@
 import json
 import os
-from pathlib import Path
 from time import perf_counter
 import yaml
 
@@ -8,13 +7,18 @@ import click
 import pandas as pd
 
 from depot_charging_optimization.config import FileConfig, OptimizerConfig
-from depot_charging_optimization.core import CasadiOptimizer, GurobiOptimizer, Optimizer
+from depot_charging_optimization.optimizer.base import Optimizer
+from depot_charging_optimization.optimizer.casadi import CasadiOptimizer
+from depot_charging_optimization.optimizer.gurobi import GurobiOptimizer
 from depot_charging_optimization.data_models import Input
 from depot_charging_optimization.logging import get_logger, log_stdout
-from depot_charging_optimization.result_store import ResultStore
 
 
-def run_main(
+@click.command()
+@click.option("--debug", is_flag=True, default=False, help="print debug messages")
+@FileConfig.as_click_options
+@OptimizerConfig.as_click_options
+def main(
     debug: bool,
     file_config_cli_arguments: dict,
     optimizer_config_cli_arguments: dict,
@@ -64,119 +68,56 @@ def run_main(
         logger.info(f"  {i + 1}. [cyan]{file}")
     logger.info("")
 
-    data_input = Input.combine(data)
+    input_data = Input.combine(data)
 
     energy_price = pd.read_csv(file_config.energy_price_file)
-    energy_price["energy_price"] /= 3.6e6  # convert to CHF / Joule
+    energy_price["energy_price"] /= 3.6e6   # convert to CHF / Joule
 
     grid_tariff = pd.read_csv(file_config.grid_tariff_file)
-    grid_tariff["grid_tariff"] /= 365 * 1.0e6  # convert to CHF / Watt
+    grid_tariff["grid_tariff"] /= (365 * 1.0e6)   # convert to CHF / Watt
 
-    data_input = data_input.add_energy_price(
-        energy_price["time"].to_list(), energy_price["energy_price"].to_list()
-    )
-    data_input = data_input.add_grid_tariff(grid_tariff["grid_tariff"][0])
+    input_data = input_data.add_energy_price(energy_price["time"].to_list(), energy_price["energy_price"].to_list())
+    input_data = input_data.add_grid_tariff(grid_tariff["grid_tariff"][0])
 
     # optimization
-    start = perf_counter()
-    optimizer: Optimizer
-    if optimizer_config.optimizer_type == "casadi":
-        optimizer = CasadiOptimizer(
-            data_input,
-            config=optimizer_config,
-        )
-    else:
-        optimizer = GurobiOptimizer(
-            data_input,
-            config=optimizer_config,
-        )
+    optimizer: Optimizer | None = None
+    match optimizer_config.optimizer_type:
+        case "casadi":
+            optimizer = CasadiOptimizer(input_data, config=optimizer_config)
+        case "gurobi":
+            optimizer = GurobiOptimizer(input_data, config=optimizer_config)
+        case _:
+            logger.error(f"Unknown optimizer type: {optimizer_config.optimizer_type}")
+            return
 
-    optimizer.build(
-        ce_function_type=optimizer_config.ce_function_type, alpha=optimizer_config.alpha
-    )
+    assert optimizer is not None
 
-    # solve
+    build_start = perf_counter()
+    optimizer.build()
+    build_time = perf_counter() - build_start
+
     with log_stdout(logger, level="debug"):
+        solve_start = perf_counter()
         solution = optimizer.solve()
-    optimization_time = perf_counter() - start
+        solve_time = perf_counter() - solve_start
+
+    logger.info(f"Optimizer build time: {build_time:.4f} seconds")
+    logger.info(f"Optimizer solve time: {solve_time:.4f} seconds")
 
     if solution is None:
-        logger.error("No solution found")
+        logger.error("No feasible solution found")
     else:
-        # slack information
-        max_slack = 0
-        max_slack_location = None
-        for sublist in optimizer.slack["state_of_energy"]:
-            if max(sublist) > max_slack:
-                max_slack = max(sublist)
-                max_slack_location = "State of Energy"
-        for sublist in optimizer.slack["lower_soe_envelope"]:
-            if max(sublist) > max_slack:
-                max_slack = max(sublist)
-                max_slack_location = "Lower SoE Envelope"
-        for sublist in optimizer.slack["charging_power"]:
-            if max(sublist) > max_slack:
-                max_slack = max(sublist)
-                max_slack_location = "Charging Power"
-        max_slack = max(max_slack, optimizer.slack["max_charging_power"])
-        if optimizer.slack["max_charging_power"] > max_slack:
-            max_slack = optimizer.slack["max_charging_power"]
-            max_slack_location = "Max Charging Power"
-        logger.debug(
-            f"Maximum slack: {max_slack:.3e} (found in [{max_slack_location}] constraints)"
-        )
-
-        # solution information
-        logger.info(f"Found solution in {optimization_time:.4f} seconds")
         total_cost = f"{solution.total_cost:.3f} $"
         energy_cost = f"{solution.energy_cost:.3f} $"
         power_cost = f"{solution.power_cost:.3f} $"
+
         max_cost_string_length = max(map(len, [total_cost, energy_cost, power_cost]))
-        logger.info(
-            f"Total cost of solution:   {' ' * (max_cost_string_length - len(total_cost))}{total_cost}"
-        )
-        logger.info(
-            f"Energy cost of solution:  {' ' * (max_cost_string_length - len(energy_cost))}{energy_cost}"
-        )
-        logger.info(
-            f"Power cost of solution:   {' ' * (max_cost_string_length - len(power_cost))}{power_cost}"
-        )
+        logger.info(f"Total cost of solution:   {' ' * (max_cost_string_length - len(total_cost))}{total_cost}")
+        logger.info(f"Energy cost of solution:  {' ' * (max_cost_string_length - len(energy_cost))}{energy_cost}")
+        logger.info(f"Power cost of solution:   {' ' * (max_cost_string_length - len(power_cost))}{power_cost}")
 
         solution_dir = os.path.dirname(file_config.solution_file)
         os.makedirs(solution_dir, exist_ok=True)
         with open(file_config.solution_file, "w") as f:
             f.write(solution.model_dump_json(indent=4))
         logger.info(f"Saved solution to [cyan3]{file_config.solution_file}")
-
-        result_store = ResultStore(Path("logs/optimize.log"))
-        result_store.write(
-            {
-                "solution_total_cost": solution.total_cost,
-                "input": {
-                    "data_files": list(map(str, file_config.data_files)),
-                    "energy_price_file": str(file_config.energy_price_file),
-                    "ce_function": optimizer_config.ce_function_type,
-                    "alpha": optimizer_config.alpha,
-                    "solution_file": str(file_config.solution_file),
-                    "bidirectional_charging": optimizer_config.bidirectional_charging,
-                    "optimizer_type": optimizer_config.optimizer_type,
-                    "debug": debug,
-                },
-            }
-        )
-
-
-@click.command()
-@click.option("--debug", is_flag=True, default=False, help="print debug messages")
-@FileConfig.as_click_options
-@OptimizerConfig.as_click_options
-def main(
-    debug: bool,
-    file_config: FileConfig,
-    optimizer_config: OptimizerConfig,
-):
-    return run_main(
-        debug=debug,
-        file_config=file_config,
-        optimizer_config=optimizer_config,
-    )
